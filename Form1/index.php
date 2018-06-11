ec166475e539cc023a2be11bd1fabe1b16398773
rashedul-alam <26365486+rashedul-alam@users.noreply.github.com>
2018-06-11 10:34:06 +0600

    Add files via upload

---
+++ Form1/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		
+			<h1>Sign Up</h1><br>
+			<p>Its Free And Always Will Be</p><br>
+			
+			<table style="width:50%">
+			
+				<tr>
+					<td><input type ="text" name="fn" placeholder ="First Name"></td>
+					<td><input type ="text" name="ln" placeholder ="Last Name"></td>
+				
+				</tr>
+				<tr>
+					<td colspan="2"><input type ="text" name="mail" placeholder ="Your Email"></td>
+				
+				
+				</tr>
+				<tr>
+					<td colspan="2"><input type ="text" name="rmail" placeholder ="Re-enter Email"></td>
+				
+				
+				</tr>
+				
+				<tr>
+					<td colspan="2"><input type ="text" name="pass" placeholder ="New Password"></td>
+				
+				
+				</tr>
+				<tr>
+					<td colspan="2"><h2>BirthDay:</h2></td>
+				
+				
+				</tr>
+				<tr>
+					<td>
+						<select>
+							<option value="Jan">Jan</option>
+							<option value="Feb">Feb</option>
+							<option value="Mar">Mar</option>
+							<option value="Apr">Api</option>
+						</select>
+					</td>
+				
+				
+				</tr>
+				
+				
+				
+  
+			</table>
+			
+		<?php	
+			
+			
+		?>
+
+
+	</body>
+</html>
